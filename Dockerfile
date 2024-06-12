d2e05b8d77cedca02d0b8939dcd82f92beb542c3
John Doe <dev.adipurnomo@gmail.com>
2024-06-12 17:19:00 +0700

    feat: add bonus points and add improvements

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,15 @@
-# Use the official Python image from the Docker Hub
-FROM python:3.10-slim
+FROM python:3.11.9-bullseye
 
-# Set environment variables
-ENV PYTHONUNBUFFERED True
+RUN apt update 
 
-# Install Chrome dependencies
-RUN apt-get update && apt-get install -y \
-    curl \
-    gnupg \
-    unzip \
-    && rm -rf /var/lib/apt/lists/*
-
-# Install Chrome
-RUN curl -sS -L -o /tmp/google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-    && apt-get -y install /tmp/google-chrome.deb \
-    && rm /tmp/google-chrome.deb
-
-# Install ChromeDriver
-RUN CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` \
-    && curl -sS -L -o /tmp/chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip \
-    && unzip /tmp/chromedriver_linux64.zip -d /usr/local/bin/ \
-    && rm /tmp/chromedriver_linux64.zip
-
-# Install application dependencies
-COPY requirements.txt .
-RUN pip install -r requirements.txt
-
-# Copy the application files
-COPY . /app
 WORKDIR /app
 
-# Command to run the application
-CMD exec gunicorn --bind :$PORT --workers $GUNICORN_PROCESSES --threads $GUNICORN_THREADS --timeout 120 app:app
+COPY requirements.txt .
+
+RUN pip3 install -r /app/requirements.txt
+
+COPY templates /app/templates/
+COPY app.py /app/
+COPY wsgi.py /app/
+
+CMD ["gunicorn", "--bind","0.0.0.0:5000", "wsgi:app", "--timeout", "600", "--workers", "5", "--worker-connections=1000"]
--- app.py
+++ app.py
@@ -1,139 +1,152 @@
-from flask import Flask, render_template, request, jsonify
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-forwarded_allow_ips = '*'
-
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+from flask import Flask, render_template, request
+import requests
+from datetime import datetime
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-def check_badges(profile_url, badge_list):
-    # Set up the WebDriver (Make sure you have the ChromeDriver installed and set in your PATH)
-    chrome_driver_path = '/usr/local/bin/chromedriver'
-    service = Service(chrome_driver_path)
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run in headless mode if you don't need a browser UI
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+def check_profile(profile_url):
+    try:
+        response = requests.get(profile_url)
+        response.raise_for_status()
+        return response.status_code
+    except requests.RequestException:
+        return None
 
-    # Open the Qwiklabs public profile
-    driver.get(profile_url)
+def check_badges(profile_url, badge_list):
+    response = requests.get(profile_url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, "html.parser")
+    results = soup.find(id="jump-content")
+    profile_badges = results.find_all("div", class_="profile-badges")
 
     results = []
     arcade_badges_earned = 0
     course_badges_earned = 0
     skill_badges_earned = 0
     total_points = 0
-    for badge_type, badge_name, badge_url in badge_list:
-        try:
-            badge_elements = driver.find_elements(By.CSS_SELECTOR, "span.ql-title-medium.l-mts")
-            arcade_points = 1
-            courses_points = 0
-            skill_badge_points = 0.5
-            found = False
-            for element in badge_elements:
-                if element.text == badge_name:
+
+    for badge_type, badge_label, badge_name, badge_url in badge_list:
+        arcade_points = 1
+        courses_points = 0
+        skill_badge_points = 0.5
+        found = False
+
+        for profile_badge in profile_badges:
+            spawn_profile_badge = profile_badge.find_all("span", class_="ql-title-medium")
+            for profile_badge in spawn_profile_badge:
+                badge_title = profile_badge.text.strip()
+                if badge_title == badge_name:
                     found = True
                     break
-            points = arcade_points if badge_type == "Arcade Game" else skill_badge_points if badge_type == "Skill Badges" else courses_points if badge_type == "Course" else 0
-            if found:
-                total_points += points
-                if badge_type == "Arcade Game":
-                    arcade_badges_earned += 1
-                if badge_type == "Course":
-                    course_badges_earned += 1
-                if badge_type == "Skill Badges":
-                    skill_badges_earned += 1
-            results.append(( badge_type, badge_name, badge_url, points, '✅' if found else '❌'))
-        except Exception as e:
-            results.append((badge_type, badge_name, 0, '❌'))
-    # Close the WebDriver
-    driver.quit()
-    return results, arcade_badges_earned, skill_badges_earned, course_badges_earned, total_points
+
+        points = arcade_points if badge_type == "Arcade Game" else skill_badge_points if badge_type == "Skill Badges" else courses_points if badge_type == "Course" else 0
+        if found:
+            total_points += points
+            if badge_type == "Arcade Game":
+                arcade_badges_earned += 1
+            if badge_type == "Course":
+                course_badges_earned += 1
+            if badge_type == "Skill Badges":
+                skill_badges_earned += 1
+        results.append((badge_type, badge_label, badge_name, badge_url, points, '✅' if found else '❌'))
+
+    return results, arcade_badges_earned, skill_badges_earned, course_badges_earned, total_points, None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-      profile_url = request.form['profileUrl']
-      tier_results = []
-      cloud_results = []
-      ai_results = []
-      total_points = 0
-      
-      cloud_badges = [
-          ("Arcade Game", "Level 1: Security and Compliance", "https://example.com/level1"),
-          ("Arcade Game", "Level 2: Data with BigQuery", "https://example.com/level2"),
-          ("Arcade Game", "Level 1: Cloud Networking", "https://example.com/level1_networking"),
-          ("Arcade Game", "Level 2: Develop and Deploy", "https://example.com/level2_deploy"),
-          ("Arcade Game", "Level 1: Arcade Juli - Coming Soon", "https://example.com/level2_deploy"),
-          ("Arcade Game", "Level 2: Arcade Juli - Coming Soon", "https://example.com/level2_deploy"),
-          ("Course", "Google Cloud Computing Foundations: Cloud Computing Fundamentals", "https://example.com/foundations1"),
-          ("Course", "Google Cloud Computing Foundations: Infrastructure in Google Cloud", "https://example.com/foundations2"),
-          ("Course", "Google Cloud Computing Foundations: Networking & Security in Google Cloud", "https://example.com/foundations3"),
-          ("Course", "Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud", "https://example.com/foundations4"),
-          ("Skill Badges", "Implement Load Balancing on Compute Engine", "https://example.com/load_balancing"),
-          ("Skill Badges", "Set Up an App Dev Environment on Google Cloud", "https://example.com/app_dev"),
-          ("Skill Badges", "Build a Secure Google Cloud Network", "https://example.com/secure_network"),
-          ("Skill Badges", "Prepare Data for ML APIs on Google Cloud", "https://example.com/ml_apis"),
-          ("Skill Badges", "Set Up a Google Cloud Network", "https://example.com/cloud_network"),
-          ("Skill Badges", "Implement Cloud Security Fundamentals on Google Cloud", "https://example.com/security_fundamentals"),
-          ("Skill Badges", "Get Started with Cloud Storage", "https://example.com/cloud_storage"),
-          ("Skill Badges", "The Basics of Google Cloud Compute", "https://example.com/cloud_compute")
-      ]
+        profile_url = request.form['profileUrl']
+        status_code = check_profile(profile_url)
+        tier_results = []
+        errors = []
+        cloud_bonus_points = 0.0
+        ai_bonus_points = 0.0
+        track_bonus_points = 0.0
 
-      ai_badges = [
-          ("Arcade Game", "Level 3: GenAIus Registries", "https://example.com/genaius_registries"),
-          ("Arcade Game", "Level 3: GenAIus Travels", "https://example.com/genaius_travels"),
-          ("Arcade Game", "Level 3: Coming Soon", "https://example.com/genaius_travels"),
-          ("Course", "Introduction to Generative AI", "https://example.com/generative_ai"),
-          ("Course", "Introduction to Large Language Models", "https://example.com/large_language_models"),
-          ("Course", "Introduction to Responsible AI", "https://example.com/responsible_ai"),
-          ("Course", "Responsible AI: Applying AI Principles with Google Cloud", "https://example.com/applying_ai_principles"),
-          ("Skill Badges", "Prompt Design in Vertex AI", "https://example.com/prompt_design"),
-          ("Skill Badges", "Classify Images with TensorFlow on Google Cloud", "https://example.com/classify_images"),
-          ("Skill Badges", "Build and Deploy Machine Learning Solutions on Vertex AI", "https://example.com/ml_solutions"),
-          ("Skill Badges", "Analyze Speech and Language with Google APIs", "https://example.com/speech_language"),
-          ("Skill Badges", "Create ML Models with BigQuery ML", "https://example.com/bigquery_ml"),
-          ("Skill Badges", "Derive Insights from BigQuery Data", "https://example.com/insights_bigquery"),
-          ("Skill Badges", "Detect Manufacturing Defects using Visual Inspection AI", "https://example.com/visual_inspection"),
-          ("Skill Badges", "Analyze Sentiment with Natural Language API", "https://example.com/sentiment_analysis")
-      ]
+        if status_code == 200:
+            cloud_badges = [
+                ("Arcade Game", "Level 1: Security and Compliance (1q-secure-0545)", "Level 1: Security and Compliance", "https://www.cloudskillsboost.google/games/5058"),
+                ("Arcade Game", "Level 2: Data with BigQuery (1q-bigquery-0544)", "Level 2: Data with BigQuery", "https://www.cloudskillsboost.google/games/5057"),
+                ("Arcade Game", "Level 1: Cloud Networking (1q-networking-0631)", "Level 1: Cloud Networking", "https://www.cloudskillsboost.google/games/5156"),
+                ("Arcade Game", "Level 1: Cloud Networking (1q-deploy-0632)", "Level 2: Develop and Deploy", "https://www.cloudskillsboost.google/games/5155"),
+                ("Arcade Game", "Level 1: Arcade Juli - Coming Soon", "Level 1: Arcade Juli - Coming Soon", "https://www.cloudskillsboost.google/games/"),
+                ("Arcade Game", "Level 2: Arcade Juli - Coming Soon", "Level 2: Arcade Juli - Coming Soon", "https://www.cloudskillsboost.google/games/"),
+                ("Course", "Google Cloud Computing Foundations: Cloud Computing Fundamentals", "Google Cloud Computing Foundations: Cloud Computing Fundamentals", "https://www.cloudskillsboost.google/course_templates/153"),
+                ("Course", "Google Cloud Computing Foundations: Infrastructure in Google Cloud", "Google Cloud Computing Foundations: Infrastructure in Google Cloud", "https://www.cloudskillsboost.google/course_templates/154"),
+                ("Course", "Google Cloud Computing Foundations: Networking & Security in Google Cloud", "Google Cloud Computing Foundations: Networking & Security in Google Cloud", "https://www.cloudskillsboost.google/course_templates/155"),
+                ("Course", "Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud", "Google Cloud Computing Foundations: Data, ML, and AI in Google Cloud", "https://www.cloudskillsboost.google/course_templates/156"),
+                ("Skill Badges", "Implement Load Balancing on Compute Engine", "Implement Load Balancing on Compute Engine", "https://www.cloudskillsboost.google/course_templates/648"),
+                ("Skill Badges", "Set Up an App Dev Environment on Google Cloud", "Set Up an App Dev Environment on Google Cloud", "https://www.cloudskillsboost.google/course_templates/637"),
+                ("Skill Badges", "Build a Secure Google Cloud Network", "Build a Secure Google Cloud Network", "https://www.cloudskillsboost.google/course_templates/654"),
+                ("Skill Badges", "Prepare Data for ML APIs on Google Cloud", "Prepare Data for ML APIs on Google Cloud", "https://www.cloudskillsboost.google/course_templates/631"),
+                ("Skill Badges", "Set Up a Google Cloud Network", "Set Up a Google Cloud Network", "https://www.cloudskillsboost.google/course_templates/641"),
+                ("Skill Badges", "Implement Cloud Security Fundamentals on Google Cloud", "Implement Cloud Security Fundamentals on Google Cloud", "https://www.cloudskillsboost.google/course_templates/645"),
+                ("Skill Badges", "Get Started with Cloud Storage", "Get Started with Cloud Storage", "https://www.cloudskillsboost.google/course_templates/725"),
+                ("Skill Badges", "The Basics of Google Cloud Compute", "The Basics of Google Cloud Compute", "https://www.cloudskillsboost.google/course_templates/754")
+            ]
 
-      cloud_results, cloud_arcade_badges, skill_badges, course_badges, cloud_points = check_badges(profile_url, cloud_badges)
-      ai_results, ai_arcade_badges, skill_badges, course_badges, ai_points = check_badges(profile_url, ai_badges)
+            ai_badges = [
+                ("Arcade Game", "Level 3: GenAIus Registries (1q-deploy-0632)", "Level 3: GenAIus Registries", "https://www.cloudskillsboost.google/games/5060"),
+                ("Arcade Game", "Level 3: GenAIus Travels (1q-genai-0631)", "Level 3: GenAIus Travels", "https://www.cloudskillsboost.google/games/5154"),
+                ("Arcade Game", "Level 3: Coming Soon", "Level 3: Coming Soon", "https://www.cloudskillsboost.google/games/"),
+                ("Course", "Introduction to Generative AI", "Introduction to Generative AI", "https://www.cloudskillsboost.google/course_templates/536"),
+                ("Course", "Introduction to Large Language Models", "Introduction to Large Language Models", "https://www.cloudskillsboost.google/course_templates/539"),
+                ("Course", "Introduction to Responsible AI", "Introduction to Responsible AI", "https://www.cloudskillsboost.google/course_templates/554"),
+                ("Course", "Responsible AI: Applying AI Principles with Google Cloud", "Responsible AI: Applying AI Principles with Google Cloud", "https://www.cloudskillsboost.google/course_templates/388"),
+                ("Skill Badges", "Prompt Design in Vertex AI", "Prompt Design in Vertex AI", "https://www.cloudskillsboost.google/course_templates/976"),
+                ("Skill Badges", "Get Started with TensorFlow on Google Cloud", "Classify Images with TensorFlow on Google Cloud", "https://www.cloudskillsboost.google/course_templates/646"),
+                ("Skill Badges", "Build and Deploy Machine Learning Solutions on Vertex AI", "Build and Deploy Machine Learning Solutions on Vertex AI", "https://www.cloudskillsboost.google/course_templates/684"),
+                ("Skill Badges", "Analyze Speech and Language with Google APIs", "Analyze Speech and Language with Google APIs", "https://www.cloudskillsboost.google/course_templates/634"),
+                ("Skill Badges", "Create ML Models with BigQuery ML", "Create ML Models with BigQuery ML", "https://www.cloudskillsboost.google/course_templates/626"),
+                ("Skill Badges", "Derive Insights from BigQuery Data", "Derive Insights from BigQuery Data", "https://www.cloudskillsboost.google/course_templates/623"),
+                ("Skill Badges", "Detect Manufacturing Defects using Visual Inspection AI", "Detect Manufacturing Defects using Visual Inspection AI", "https://www.cloudskillsboost.google/course_templates/644"),
+                ("Skill Badges", "Analyze Sentiment with Natural Language API", "Analyze Sentiment with Natural Language API", "https://www.cloudskillsboost.google/course_templates/667")
+            ]
 
-      total_points = ai_points + cloud_points
-      
-      # minimum badges
-      cloud_arcade_valid = cloud_arcade_badges >= 4
-      ai_arcade_valid = ai_arcade_badges >= 2
-      skill_valid = skill_badges == 8
-      course_valid = course_badges == 4
+            cloud_results, cloud_arcade_badges, cloud_skill_badges, cloud_course_badges, cloud_points, cloud_error = check_badges(profile_url, cloud_badges)
+            ai_results, ai_arcade_badges, ai_skill_badges, ai_course_badges, ai_points, ai_error = check_badges(profile_url, ai_badges)
 
-      # Check if all badges are valid
-      cloud_valid = cloud_arcade_valid and skill_valid and course_valid
-      ai_valid = ai_arcade_valid and skill_valid and course_valid
+            cloud_arcade_valid = cloud_arcade_badges >= 4
+            ai_arcade_valid = ai_arcade_badges >= 2
+            cloud_skill_valid = cloud_skill_badges >= 8
+            ai_skill_valid = ai_skill_badges >= 8
+            cloud_course_valid = cloud_course_badges >= 4
+            ai_course_valid = ai_course_badges == 4
 
-      if cloud_valid and ai_valid:
-          tier_result = "you got Tier Advance 🎉🥳"
-          tier_color = "success"
-      elif cloud_valid or ai_valid:
-          tier_result = "You got tier Standard 🥳, work harder to get next milestone, keep it up bro 🔥"
-          tier_color = "info"
-      else:
-          tier_result = "You got no Tier rn, keep it up bro 💔"
-          tier_color = "danger"
-      
-      tier_results.append(( tier_result, tier_color ))
-      return render_template('index.html', cloud_results=cloud_results, ai_results=ai_results, tier_results=tier_results, ai_points=ai_points, cloud_points=cloud_points, total_points=total_points, profile_url=profile_url)
+            cloud_valid = cloud_arcade_valid and cloud_skill_valid and cloud_course_valid
+            ai_valid = ai_arcade_valid and ai_skill_valid and ai_course_valid
+
+            if cloud_valid and ai_valid:
+                tier_result = "Tier Advance (Milestone #2) 🎉🥳"
+                tier_color = "success"
+                ai_bonus_points = 4.0
+                cloud_bonus_points = 2.0
+                track_bonus_points = 5.0
+            elif cloud_valid:
+                tier_result = "Tier Standard (Milestone #1) 🥳, work harder to achieve the Milestone #2, keep it up bro 🔥"
+                tier_color = "info"
+                ai_bonus_points = ai_bonus_points
+                cloud_bonus_points = 2.0
+                track_bonus_points = track_bonus_points
+            elif ai_valid:
+                tier_result = "Tier Standard 🥳, work harder to achieve the Milestone #2, keep it up bro 🔥"
+                tier_color = "info"
+                cloud_bonus_points = cloud_bonus_points
+                ai_bonus_points = 4.0
+                track_bonus_points = track_bonus_points
+            else:
+                tier_result = "No Tier. Keep it up bro 🔥"
+                tier_color = "danger"
+
+            tier_results.append((tier_result, tier_color))
+            return render_template('index.html', cloud_results=cloud_results, ai_results=ai_results, tier_results=tier_results, ai_points=ai_points, cloud_points=cloud_points, ai_bonus_points=ai_bonus_points, cloud_bonus_points=cloud_bonus_points, track_bonus_points=track_bonus_points, profile_url=profile_url)
+        elif status_code == None:
+            error_message="An error occurred while checking url. Please make sure the profile URL is correct."
+            error_color= "danger"
+            errors.append((error_message, error_color))
+        return render_template('index.html', profile_url=profile_url, errors=errors)
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
